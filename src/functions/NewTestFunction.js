aa4ea551ffc13d70f0da19b655e545a1e57f0406
Nitin Rastogi <nitin.rastogi@live.in>
2024-06-05 20:11:35 -0400

    Function Code

---
+++ src/functions/NewTestFunction.js
@@ -0,0 +1,13 @@
+const { app } = require('@azure/functions');
+
+app.http('NewTestFunction', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Test - Hello, ${name}!` };
+    }
+});
